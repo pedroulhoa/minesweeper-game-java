8d8e9370d3b185788479d1a7cbb2622737824944
Pedro Ulhoa <pedroulhoa@live.com>
2021-03-16 21:28:01 -0300

    add field entity

--- src/br/com/app/cm/model/Field.java
+++ src/br/com/app/cm/model/Field.java
@@ -1,7 +1,41 @@
 package br.com.app.cm.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Field {
 
+    private boolean mine = false;
+    private boolean open = false;
+    private boolean marked = false;
 
+    private final int line;
+    private final int column;
 
+    private List<Field> neighbors = new ArrayList<>();
+
+    Field(int line, int column) {
+        this.line = line;
+        this.column = column;
+    }
+
+    boolean addNeighbor(Field neighbor) {
+        boolean differentLine = line != neighbor.line;
+        boolean differentColumn = column != neighbor.column;
+        boolean diagonal = differentLine && differentColumn;
+
+        int deltaLine = Math.abs(line - neighbor.line);
+        int deltaColumn = Math.abs(column - neighbor.column);
+        int deltaGeneral = deltaColumn + deltaLine;
+
+        if (deltaGeneral == 1 && !diagonal) {
+            neighbors.add(neighbor);
+            return true;
+        } else if (deltaGeneral == 2 && diagonal) {
+            neighbors.add(neighbor);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
